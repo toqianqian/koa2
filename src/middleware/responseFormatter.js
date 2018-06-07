3403e194c545759c0530cb960f7cbcb9701837e2
qianqian <qian.qian@zyxr.com>
2018-06-07 19:06:14 +0800

    修改

--- src/middleware/responseFormatter.js
+++ src/middleware/responseFormatter.js
@@ -1,24 +1,31 @@
-/*
-    响应结果格式化
-    在app.use(router)之前调用
-*/
+/**
+ * 响应结果格式化
+ * 在app.use(router)之前调用
+ */
 
 const responseFormatter = async (ctx, next) => {
     // 先去执行路由
     await next()
+    console.log('ctx = ', ctx.body);
 
-    if (ctx.body) {
-        ctx.body = {
-            code: 0,
-            message: 'success',
-            data: ctx.body
-        }
-    } else {
-        ctx.body = {
-            code: 0,
-            message: 'success'
-        }
+    ctx.body = {
+        code: 0,
+        msg: 'success',
+        data: ctx.body
     }
+
+    // if (ctx.body) {
+    //     ctx.body = {
+    //         code: 0,
+    //         message: 'success',
+    //         data: ctx.body
+    //     }
+    // } else {
+    //     ctx.body = {
+    //         code: 0,
+    //         message: 'success'
+    //     }
+    // }
 }
 
 export default responseFormatter
